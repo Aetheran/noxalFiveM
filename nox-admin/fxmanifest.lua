d4fbfe72221cb066924b7b045c6e951ebb1312e3
Aetheran <chaosworld.harlan@gmail.com>
2021-05-18 04:22:44 -0700

    Functions
    Added car spawning and spawn point as well as hotfix to removing cops

--- nox-admin/fxmanifest.lua
+++ nox-admin/fxmanifest.lua
@@ -4,7 +4,8 @@ game 'gta5'
 client_scripts {
     '@menuv/menuv.lua',
     'weaponMenu.lua',
-    'nox-menu.lua'
+    'nox-menu.lua',
+    'nox-spawn.lua'
 }
 
 dependencies {
--- nox-admin/nox-spawn.lua
+++ nox-admin/nox-spawn.lua
@@ -1,4 +1,4 @@
-local spawnPos = vector3(799.4449, -1049.533, 26.97255)
+local spawnPos = vector3(799.4449, -1049.533, 26.97255) -- ls customs delivery depot
 
 AddEventHandler('onClientGameTypeStart', function()
     exports.spawnmanager:setAutoSpawnCallback(function()
@@ -17,3 +17,31 @@ AddEventHandler('onClientGameTypeStart', function()
     exports.spawnmanager:setAutoSpawn(true)
     exports.spawnmanager:forceRespawn()
 end)
+
+RegisterCommand('car', function(source, args)
+    -- account for the argument not being passed
+    local vehicleName = args[1] or 'adder'
+    -- check if the vehicle actually exists
+    if not IsModelInCdimage(vehicleName) or not IsModelAVehicle(vehicleName) then
+        TriggerEvent('chat:addMessage', {
+            args = { 'Spawned'..vehicleName..'' }
+        })
+
+        return
+    end
+    RequestModel(vehicleName) -- load the model
+    while not HasModelLoaded(vehicleName) do -- wait for the model to load
+        Wait(500) 
+    end
+
+    local playerPed = PlayerPedId() -- get the local player ped
+    local pos = GetEntityCoords(playerPed) -- get the position of the local player ped
+    -- create the vehicle
+    local vehicle = CreateVehicle(vehicleName, pos.x, pos.y, pos.z, GetEntityHeading(playerPed), true, false)
+    SetPedIntoVehicle(playerPed, vehicle, -1) -- set the player ped into the vehicle's driver seat
+    SetEntityAsNoLongerNeeded(vehicle) -- give the vehicle back to the game 
+    SetModelAsNoLongerNeeded(vehicleName) -- release the model
+    TriggerEvent('chat:addMessage', {
+		args = { 'Enjoy your new ^*' .. vehicleName .. '!' }
+	})
+end, false)
