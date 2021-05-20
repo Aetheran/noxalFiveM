e86632f6f144e748dcc56768e09277d04d9ae693
Aetheran <chaosworld.harlan@gmail.com>
2021-05-20 01:56:05 -0700

    Update nox-mph.lua

--- nox-admin/nox-mph.lua
+++ nox-admin/nox-mph.lua
@@ -1,10 +1,12 @@
 --variables
-x1 = 0.016 --title
+x1 = 0.016 --title-mph
 y1 = 0.843
 x2 = 0.016 --mph
 y2 = 0.803
 x3 = 0.016 --rpm
 y3 = 0.883
+x4 = 0.016 --gear
+y4 = 0.883
 
 --functions
 
@@ -30,21 +32,34 @@ function drawTitle(var, x, y) --Title
     DrawText(x, y)
 end
 
+function txtDraw(x, y, width, height, scale, var, r, g, b, a) --this might be cleaner idk
+    SetTextFont(2)
+    SetTextScale(scale, scale)
+    SetTextColour(r, g, b, a)
+    SetTextDropShadow(0, 0, 0, 0, 255)
+    SetTextEntry("STRING")
+    AddTextComponentString(var)
+    DrawText(x - width/2, y - height/2 + 0.005)
+end
+
 --main thread
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(1)
         local speed = 0
+        local gear = 0
         local rpm = 0
         speed = GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false))*2.236936 --constant for conversion to mph
+        gear = GetVehicleCurrentGear(GetVehiclePedIsIn(-1), false)
         --rpm = GetVehicleClutch(GetVehiclePedIsIn(GetPlayerPed(-1), false))
-        if rpm < 0.2 then rpm = 0.2 end
+        --if rpm < 0.2 then rpm = 0.2 end
         
         if(IsPedInAnyVehicle(GetPlayerPed(-1), false)) then
             drawVar("~g~" .. math.ceil(speed), x1, y1)
             drawTitle("MPH", x2, y2)
             --drawVar("~r~" .. math.abs(rpm)*6000, x3, y3)
+            drawVar("~r~" .. gear, x4, y4)
         end
     end
 end)
